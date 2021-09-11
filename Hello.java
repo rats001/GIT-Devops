8a33beb737dcce9e89b5af1b8a6f960022d29d1b
rats001 <71597684+rats001@users.noreply.github.com>
2021-09-11 15:14:13 +0530

    updated the code

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
-public class Hello{
-public static void main(String{} arg){
- system.out.println("Hello world");
+Public Class Hello{
+Public Static Void Main(String{} arg){
+ System.out.println("Hello world");
 }
 
 }
