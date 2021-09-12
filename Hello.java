fb8b8a401d25d5d96cc915e522c9e17de364120d
rats001 <salunkemonish05@gmail.com>
2021-09-12 18:51:31 +0530

    java commit Demo

--- Hello.java
+++ Hello.java
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 public class Hello{
-public static void main(String{} arg){
-     system.out.println("Hello world");
- }
-}
-=======
-Public Class Hello{
-Public Static Void Main(String{} arg){
+public static void main(String arg[]){
  System.out.println("Hello world");
 }
 
 }
->>>>>>> 8a33beb737dcce9e89b5af1b8a6f960022d29d1b
